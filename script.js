7a67da3c543c3e875a19ac79ad3995ae7e8ccb0b
Nash Blount <nsblount111300@gmail.com>
2022-11-30 13:20:54 -0500

    CUSTOM COUNTDOWN FIX

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const button = document.getElementById("input-button");
-button.addEventListener("click", function handleClick(event) {
-  event.preventDefault();
 
+button.addEventListener("click", function onClick() {
   const daysLeft = document.getElementById("days-left");
   const hoursLeft = document.getElementById("hours-left");
   const minutesLeft = document.getElementById("minutes-left");
@@ -9,7 +8,7 @@ button.addEventListener("click", function handleClick(event) {
   const title = document.getElementById("title");
   const inputTitle = document.getElementById("input");
   const inputDate = document.getElementById("input-date");
-  title.innerHTML = inputTitle.value;
+  title.innerHTML = "Days until " + inputTitle.value + ":";
   const custom = new Date(inputDate.value);
 
   const second = 1000;
@@ -17,33 +16,23 @@ button.addEventListener("click", function handleClick(event) {
   const hour = minute * 60;
   const day = hour * 24; //milliseconds
   let timerId;
+  const today = new Date();
+  const timeSpan = custom - today;
+  const days = Math.floor(timeSpan / day);
+  const hours = Math.floor((timeSpan % day) / hour);
+  const minutes = Math.floor((timeSpan % hour) / minute);
+  const seconds = Math.floor((timeSpan % minute) / second);
 
-  function customCountdown() {
-    const today = new Date();
-    const timeSpan = custom - today;
-    const days = Math.floor(timeSpan / day);
-    const hours = Math.floor((timeSpan % day) / hour);
-    const minutes = Math.floor((timeSpan % hour) / minute);
-    const seconds = Math.floor((timeSpan % minute) / second);
+  daysLeft.innerHTML = days + " Days";
+  hoursLeft.innerHTML = hours + " Hours";
+  minutesLeft.innerHTML = minutes + " Minutes";
+  secondsLeft.innerHTML = seconds + " Seconds";
 
-    daysLeft.innerHTML = days + " Days";
-    hoursLeft.innerHTML = hours + " Hours";
-    minutesLeft.innerHTML = minutes + " Minutes";
-    secondsLeft.innerHTML = seconds + " Seconds";
-    if (timeSpan <= 0) {
-      title.innerHTML = "It's Today!";
-      daysLeft.innerHTML = "";
-      hoursLeft.innerHTML = "";
-      minutesLeft.innerHTML = "";
-      secondsLeft.innerHTML = "";
-      clearInterval(timerId);
-    }
-  }
-  // daysLeft.innerHTML = "";
-  // hoursLeft.innerHTML = "";
-  // minutesLeft.innerHTML = "";
-  // secondsLeft.innerHTML = "";
-  inputTitle.value = "";
-  inputDate.value = "";
-  timerId = setInterval(customCountdown, second);
+  document.getElementById("input-countdown").style.opacity = 0;
+  document.getElementById("input-countdown").style.height = 0;
+  document.getElementById("input-countdown").style.width = 0;
+
+  document.getElementById("reset-button").style.opacity = 100;
+
+  timerId = setInterval(onClick, second);
 });
