1c215494c88746e75b2bfefe7218b6c641fb0c3f
nsblount111300 <nsblount111300@gmail.com>
2022-11-29 19:09:32 -0500

    confusion/custom css

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function onSubmit() {
+const button = document.getElementById("input-button");
+button.addEventListener("click", function handleClick(event) {
+  event.preventDefault();
+
   const daysLeft = document.getElementById("days-left");
   const hoursLeft = document.getElementById("hours-left");
   const minutesLeft = document.getElementById("minutes-left");
   const secondsLeft = document.getElementById("seconds-left");
   const title = document.getElementById("title");
-
-  const custom = new Date(document.getElementById("input-date").value);
+  const inputTitle = document.getElementById("input");
+  const inputDate = document.getElementById("input-date");
+  title.innerHTML = inputTitle.value;
+  const custom = new Date(inputDate.value);
 
   const second = 1000;
   const minute = second * 60;
@@ -16,18 +21,15 @@ function onSubmit() {
   function customCountdown() {
     const today = new Date();
     const timeSpan = custom - today;
-
     const days = Math.floor(timeSpan / day);
     const hours = Math.floor((timeSpan % day) / hour);
     const minutes = Math.floor((timeSpan % hour) / minute);
     const seconds = Math.floor((timeSpan % minute) / second);
 
-    title.innerHTML = document.getElementById("input").value;
     daysLeft.innerHTML = days + " Days";
     hoursLeft.innerHTML = hours + " Hours";
     minutesLeft.innerHTML = minutes + " Minutes";
     secondsLeft.innerHTML = seconds + " Seconds";
-
     if (timeSpan <= 0) {
       title.innerHTML = "It's Today!";
       daysLeft.innerHTML = "";
@@ -37,5 +39,11 @@ function onSubmit() {
       clearInterval(timerId);
     }
   }
+  // daysLeft.innerHTML = "";
+  // hoursLeft.innerHTML = "";
+  // minutesLeft.innerHTML = "";
+  // secondsLeft.innerHTML = "";
+  inputTitle.value = "";
+  inputDate.value = "";
   timerId = setInterval(customCountdown, second);
-}
+});
