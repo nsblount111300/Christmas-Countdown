9d0700b9245e44bc17dd750e7ab1fa8fb56fe017
nsblount111300 <nsblount111300@gmail.com>
2022-11-30 21:13:19 -0500

    failing

---
+++ JS/customCD.js
@@ -0,0 +1,41 @@
+import { date, input } from "script.js";
+const daysLeft = document.getElementById("days-left");
+const hoursLeft = document.getElementById("hours-left");
+const minutesLeft = document.getElementById("minutes-left");
+const secondsLeft = document.getElementById("seconds-left");
+const title = document.getElementById("title");
+
+title.innerHTML = "Days until " + input.value + ":";
+const custom = new Date(date.value);
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24; //milliseconds
+let timerId;
+
+function christmasCountdown() {
+  const today = new Date();
+  const timeSpan = custom - today;
+
+  const days = Math.floor(timeSpan / day);
+  const hours = Math.floor((timeSpan % day) / hour);
+  const minutes = Math.floor((timeSpan % hour) / minute);
+  const seconds = Math.floor((timeSpan % minute) / second);
+
+  daysLeft.innerHTML = days + " Days";
+  hoursLeft.innerHTML = hours + " Hours";
+  minutesLeft.innerHTML = minutes + " Minutes";
+  secondsLeft.innerHTML = seconds + " Seconds";
+
+  if (timeSpan <= 0) {
+    title.innerHTML = "It's today!";
+    daysLeft.innerHTML = "";
+    hoursLeft.innerHTML = "";
+    minutesLeft.innerHTML = "";
+    secondsLeft.innerHTML = "";
+    clearInterval(timerId);
+  }
+}
+
+timerId = setInterval(christmasCountdown, second);
--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-const button = document.getElementById("input-button");
+let input = document.querySelector(".input");
+let date = document.querySelector(".date");
+let inputButton = document.querySelector(".button");
+// const button = document.getElementById("input-button");
 
-button.addEventListener("click", function onClick() {
-  const daysLeft = document.getElementById("days-left");
-  const hoursLeft = document.getElementById("hours-left");
-  const minutesLeft = document.getElementById("minutes-left");
-  const secondsLeft = document.getElementById("seconds-left");
-  const title = document.getElementById("title");
-  const inputTitle = document.getElementById("input");
-  const inputDate = document.getElementById("input-date");
-  title.innerHTML = "Days until " + inputTitle.value + ":";
-  const custom = new Date(inputDate.value);
+inputButton.disabled = true;
+input.addEventListener("change", stateHandle);
+date.addEventListener("change", stateHandle);
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24; //milliseconds
-  let timerId;
-  const today = new Date();
-  const timeSpan = custom - today;
-  const days = Math.floor(timeSpan / day);
-  const hours = Math.floor((timeSpan % day) / hour);
-  const minutes = Math.floor((timeSpan % hour) / minute);
-  const seconds = Math.floor((timeSpan % minute) / second);
+function stateHandle() {
+  if (
+    document.querySelector(".input").value === "" ||
+    document.querySelector(".date").value === ""
+  ) {
+    inputButton.disabled = true;
+  } else {
+    inputButton.disabled = false;
+  }
+}
 
-  daysLeft.innerHTML = days + " Days";
-  hoursLeft.innerHTML = hours + " Hours";
-  minutesLeft.innerHTML = minutes + " Minutes";
-  secondsLeft.innerHTML = seconds + " Seconds";
+// button.addEventListener("click", function onClick() {
+//   const daysLeft = document.getElementById("days-left");
+//   const hoursLeft = document.getElementById("hours-left");
+//   const minutesLeft = document.getElementById("minutes-left");
+//   const secondsLeft = document.getElementById("seconds-left");
+//   const inputTitle = document.getElementById("input");
+//   const inputDate = document.getElementById("input-date");
+//   const title = document.getElementById("title");
+//   // title.innerHTML = "Days until " + inputTitle.value + ":";
+//   const custom = new Date(inputDate.value);
 
-  document.getElementById("input-countdown").style.opacity = 0;
-  document.getElementById("input-countdown").style.height = 0;
-  document.getElementById("input-countdown").style.width = 0;
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24; //milliseconds
+//   const today = new Date();
+//   const timeSpan = custom - today;
+//   const days = Math.floor(timeSpan / day);
+//   const hours = Math.floor((timeSpan % day) / hour);
+//   const minutes = Math.floor((timeSpan % hour) / minute);
+//   const seconds = Math.floor((timeSpan % minute) / second);
 
-  document.getElementById("reset-button").style.opacity = 100;
+//   daysLeft.innerHTML = days + " Days";
+//   hoursLeft.innerHTML = hours + " Hours";
+//   minutesLeft.innerHTML = minutes + " Minutes";
+//   secondsLeft.innerHTML = seconds + " Seconds";
 
-  timerId = setInterval(onClick, second);
-});
+//   // document.getElementById("input-button").remove();
+
+//   // document.getElementById("input-countdown").style.opacity = 0;
+//   // document.getElementById("input-countdown").style.height = 0;
+//   // document.getElementById("input-countdown").style.width = 0;
+//   // document.getElementById("input-date").style.height = 0;
+//   // document.getElementById("input-date").style.width = 0;
+//   // // document.getElementById("input-button").style.height = 0;
+//   // // document.getElementById("input-button").style.width = 0;
+//   // // document.getElementById("input-button").style.padding = 0;
+//   // // document.getElementById("input-button").style.margin = 0;
+
+//   // document.getElementById("reset-button").style.opacity = 100;
+//   // document.getElementById("reset-button").style.margin = 50;
+
+//   setInterval(onClick, second);
+// });
